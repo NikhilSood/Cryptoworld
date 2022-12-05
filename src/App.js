e6f260e85995c41f011ef84a0233852e9b680f8b
Mehak Gupta <55204694+mehak13@users.noreply.github.com>
2022-12-05 19:51:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => (
       <div className="footer">
         <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
           <Link to="/">
-            Cryptoverse Inc.
+            Cryptoworld Inc.
           </Link> <br />
           All Rights Reserved.
         </Typography.Title>
